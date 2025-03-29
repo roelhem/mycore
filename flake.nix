f4f9159b7b1e581057e34232392ca89df848143d
roelhem <ik@roelweb.com>
2025-03-29 15:30:59 +0100

    Add format command

--- flake.nix
+++ flake.nix
@@ -21,76 +21,93 @@
       nixpkgs,
       ...
     }:
-    let
-      m = flake-parts.lib.evalFlakeModule { inherit inputs; } (
-        {
-          config,
-          moduleType,
-          withSystem,
-          ...
-        }:
-        let
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      {
+        config,
+        moduleType,
+        withSystem,
+        ...
+      }:
+      let
 
-          # The default nixpkgs library.
-          lib = nixpkgs.lib;
+        # The default nixpkgs library.
+        lib = nixpkgs.lib;
 
-        in
-        {
+      in
+      {
 
-          # Import flake modules here.
-          imports = [
-            inputs.devshell.flakeModule
-            flake-parts.flakeModules.flakeModules
-            ./nix/modules/flake/devshells.nix
-            ./nix/modules/flake/lib.nix
-            ./nix/modules/flake/autowire.nix
-            ./nix/modules/flake/defaults.nix
-            ./nix/modules/flake/docs
-          ];
+        # Import flake modules here.
+        imports = [
+          inputs.devshell.flakeModule
+          flake-parts.flakeModules.flakeModules
+          ./nix/modules/flake/devshells.nix
+          ./nix/modules/flake/lib.nix
+          ./nix/modules/flake/autowire.nix
+          ./nix/modules/flake/defaults.nix
+          ./nix/modules/flake/docs
+        ];
 
-          # Per-system attributes.
-          perSystem =
-            {
-              config,
-              self',
-              inputs',
-              pkgs,
-              system,
-              ...
-            }:
-            {
-              # apps = { };
+        # Per-system attributes.
+        perSystem =
+          {
+            config,
+            self',
+            inputs',
+            pkgs,
+            system,
+            ...
+          }:
+          {
+            # apps = { };
 
-              # packages = { };
+            # packages = { };
 
-              # checks = { };
+            # checks = { };
 
-              # devShells = { };
+            # devShells = { };
 
-              # The nix formatter used in this project. This is the package
-              # that is used when running `nix fmt`.
-              # formatter = pkgs.nixfmt-rfc-style;
-            };
-
-          # The usual flake attributes, including system-agnostic ones.
-          flake = {
-
-            lib = import ./nix/lib { inherit lib inputs; };
-
-            # overlays = { };
-
-            # templates = { };
-
-            # nixosConfigurations = { };
-
-            # nixosModules = { };
-
-            # darwinConfigurations = { };
-
-            # darwinModules = { };
+            # The nix formatter used in this project. This is the package
+            # that is used when running `nix fmt`.
+            # formatter = pkgs.nixfmt-rfc-style;
           };
-        }
-      );
-    in
-    m.config.flake;
+
+        # The usual flake attributes, including system-agnostic ones.
+        flake = {
+
+          lib = import ./nix/lib { inherit lib inputs; };
+
+          templates = {
+            default = {
+              path = ./nix/templates/default;
+              description = "Simple way to setup a personal project.";
+            };
+          };
+
+          # overlays = { };
+
+          # templates = { };
+
+          # nixosConfigurations = { };
+
+          # nixosModules = { };
+
+          # darwinConfigurations = { };
+
+          # darwinModules = { };
+        };
+      }
+    );
+
+  # Extra recommended nix configuration.
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://roelhem.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "roelhem.cachix.org-1:6UktCbfTJhgub82/RuVYKw6645qrrEwDGJMjhBQYYCA="
+    ];
+  };
 }
